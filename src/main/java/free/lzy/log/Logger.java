0623cf0923038bb2c43c519a6a02c6c0c03f1c85
whanche <whanche@126.com>
2015-11-09 14:23:40 +0800

    Lzy

--- src/main/java/free/lzy/log/Logger.java
+++ src/main/java/free/lzy/log/Logger.java
@@ -1,50 +1,52 @@
 package free.lzy.log;
 
+import org.slf4j.LoggerFactory;
+
 public class Logger {
 	
-	private org.apache.log4j.Logger logger;
+private org.slf4j.Logger logger;
 	
-	private Logger(org.apache.log4j.Logger logger) {
+	private Logger(org.slf4j.Logger logger) {
 		this.logger = logger;
 	}
 
 	public static Logger newInstance(String logger) {
-		return new Logger(org.apache.log4j.Logger.getLogger(logger));
+		return new Logger(LoggerFactory.getLogger(logger));
 	}
 
 	public static Logger newInstance(Class<?> logger) {
 		return newInstance(logger.getName());
 	}
 	
-	public void info(Object message) {
+	public void info(String message) {
 		logger.info(message);
 	}
 
-	public void info(Object message, Throwable t) {
+	public void info(String message, Throwable t) {
 		logger.info(message, t);
 	}
 
-	public void debug(Object message) {
+	public void debug(String message) {
 		logger.debug(message);
 	}
 	
-	public void debug(Object message, Throwable t) {
+	public void debug(String message, Throwable t) {
 		logger.debug(message, t);
 	}
 	
-	public void warn(Object message) {
+	public void warn(String message) {
 		logger.warn(message);
 	}
 	
-	public void warn(Object message, Throwable t) {
+	public void warn(String message, Throwable t) {
 		logger.warn(message, t);
 	}
 	
-	public void error(Object message) {
+	public void error(String message) {
 		logger.error(message);
 	}
 	
-	public void error(Object message, Throwable t) {
+	public void error(String message, Throwable t) {
 		logger.error(message, t);
 	}
 }
